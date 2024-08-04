da6ceb034cc6facb17625dbe007ee20900c6a174
oguz_ <oguz_mert_4469@hotmail.com>
2024-08-04 22:37:33 +0300

    Initial commit

---
+++ app/src/main/java/com/oxxy/aimlabs/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.oxxy.aimlabs
+
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.oxxy.aimlabs.databinding.ActivityMainBinding
+import kotlinx.coroutines.Runnable
+import kotlin.collections.ArrayList
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var score = 0
+    var imageArray = ArrayList<ImageView>()
+    var runnable = Runnable {}
+    var handler = Handler(Looper.getMainLooper())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        imageArray.add(binding.imageView)
+        imageArray.add(binding.imageView1)
+        imageArray.add(binding.imageView2)
+        imageArray.add(binding.imageView3)
+        imageArray.add(binding.imageView4)
+        imageArray.add(binding.imageView5)
+        imageArray.add(binding.imageView6)
+        imageArray.add(binding.imageView7)
+        imageArray.add(binding.imageView8)
+        imageArray.add(binding.imageView9)
+        imageArray.add(binding.imageView10)
+        imageArray.add(binding.imageView11)
+
+        hideImages()
+
+        object : CountDownTimer(15500, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                binding.timeText.text = "Time: ${millisUntilFinished/1000}"
+            }
+
+            override fun onFinish() {
+                binding.timeText.text = "Time: 0"
+                handler.removeCallbacks(runnable)
+
+                for (image in imageArray){
+                    image.visibility = View.INVISIBLE
+                }
+
+                val alert = AlertDialog.Builder(this@MainActivity)
+                alert.setTitle("Gülseye koymuşum")
+                alert.setMessage("Restart The Game?")
+                alert.setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, which ->
+                    //Restart
+                    val intentFromMain = intent
+                    finish()
+                    startActivity(intentFromMain)
+                })
+
+                alert.setNegativeButton("No", DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(this@MainActivity, "Game Over",Toast.LENGTH_LONG).show()
+                })
+
+                alert.show()
+            }
+
+        }.start()
+    }
+
+    fun hideImages(){
+
+        runnable = object : Runnable {
+            override fun run() {
+                for (image in imageArray){
+                    image.visibility = View.INVISIBLE
+                }
+
+                val random = Random
+                val randomIndex= random.nextInt(12)
+                imageArray[randomIndex].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable, 500)
+            }
+        }
+        handler.post(runnable)
+
+        for (image in imageArray){
+            image.visibility = View.INVISIBLE
+        }
+
+        val random = Random
+        val randomIndex= random.nextInt(12)
+        imageArray[randomIndex].visibility = View.VISIBLE
+    }
+
+    fun increaseScore(view:View){
+        score += 1
+        binding.scoreText.text = "Score: ${score}"
+    }
+}
